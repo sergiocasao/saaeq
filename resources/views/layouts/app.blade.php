0c19a039a423522ec189b32262fc173e4c7f79ca
Sergio Fonseca <sifonsecac@hotmail.com>
2016-12-04 01:24:31 -0600

    Corrección barra de navegación

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,9 +42,9 @@
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
+                    {{-- <ul class="nav navbar-nav">
                         &nbsp;
-                    </ul>
+                    </ul> --}}
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
