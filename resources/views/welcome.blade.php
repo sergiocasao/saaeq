b2db183f051034216236f2892198416d92780611
Sergio Fonseca <sifonsecac@hotmail.com>
2016-12-13 10:53:38 -0600

    Se quita descripción del welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -85,20 +85,21 @@
             </div>
         </div> --}}
 
-        @if (!Auth::check())
-            <div style="padding: 3em 0;background-image: url('{{asset('images/dna.jpg')}}'); color: white;background-size: cover;margin-top: -40px;">
+            <div style="padding: 3em 0;background-image: url('{{asset('images/dna.jpg')}}'); color: white;background-size: cover;margin-top: -40px;min-height: 350px;">
                 <div class="row" style="margin: 0;">
                     <div class="col-md-8 col-md-offset-2 col-xs-10 col-xs-offset-1">
-                        <h1>Sistema de Apoyo al Aprendizaje de los Elementos Quimicos.!</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        <h1>Sistema de Apoyo al Aprendizaje de elementos de la tabla periodica.</h1>
+                        {{-- <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> --}}
+                        @if (!Auth::check())
+                        <br><br><br><br><br>
                         <p>
                             <a class="btn btn-primary" href="{{ url('/register') }}" role="button">Regístrate</a>
                             <a class="btn btn-primary" href="{{ url('/login') }}" role="button">Inicia sesión</a>
                         </p>
+                        @endif
                     </div>
                 </div>
             </div>
-        @endif
 
         <div class="container">
             <div class="row">
